29a0d1e7538d26661bfc89c54d6b9572133d5ca8
jeyapraveenvj <praveenvj27051997@gmail.com>
2025-09-02 21:58:37 +0530

    added

--- tests/clapp.spec.js
+++ tests/clapp.spec.js
@@ -1,10 +1,21 @@
 
+
+const { test, expect } = require('@playwright/test');
+ 
+ 
+ 
  
 test('@Web Client App login', async ({ page }) => {
    //js file- Login js, DashboardPage
    const email = "anshika@gmail.com";
+   //const productName = 'zara coat 3';
+   //const products = page.locator(".card-body");
+   await page.goto("https://rahulshettyacademy.com/client");
+ await page.waitForSelector('#userEmail');
+   await page.locator("#userEmail").fill(email);
+   await page.locator("#userPassword").fill("Iamking@000");
+   await page.locator("[value='Login']").click();
+   await page.waitForLoadState('networkidle');
 
 
 })
-
-
